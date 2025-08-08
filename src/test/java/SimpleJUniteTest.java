5d99aab0fb0d4f2aa611458904868b38896d39c0
Vasili Pl <w2e3r4t51982@gmail.com>
2025-08-08 16:25:39 +0200

    Working version with mandatory fields only

--- src/test/java/SimpleJUniteTest.java
+++ src/test/java/SimpleJUniteTest.java
@@ -1,16 +1,43 @@
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleJUniteTest {
 
-@Test
-void simpleTest() {
-    Assertions.assertTrue(3>2);
-        }
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @AfterAll
+    static void afterAll() {
+        closeWebDriver(); // Закрывает браузер и очищает WebDriver
+    }
 
 
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Basil");
+        $("#lastName").setValue("Pupkin");
+        $("#userEmail").setValue("pupkin@basil.com");
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue("0441234567");
+        $("#submit").scrollTo().click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //$("#output #email").shouldHave(text("alex@egorov.com"));
+        //$("#output #currentAddress").shouldHave(text("Some street 1"));
+        //$("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
 }
