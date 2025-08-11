bdac9ecb12b5d0105e4e9e37952754db7dd5eda7
Vasili Pl <w2e3r4t51982@gmail.com>
2025-08-11 17:39:35 +0200

    Fixed 1. .idea; 2. delete banners; 3. checking cell by cell

--- src/test/java/SimpleJUniteTest.java
+++ src/test/java/SimpleJUniteTest.java
@@ -1,14 +1,10 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
-import static com.codeborne.selenide.CollectionCondition.exactTexts;
-import static com.codeborne.selenide.Condition.interactable;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleJUniteTest {
@@ -18,78 +14,85 @@ public class SimpleJUniteTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        Configuration.timeout = 5000;
     }
 
     @AfterAll
     static void afterAll() {
-        closeWebDriver(); // Закрывает браузер и очищает WebDriver
+        closeWebDriver();
     }
-
 
     @Test
     void fillFormTest() {
-
-
         open("/automation-practice-form");
+
+        // Удаляем мешающие элементы
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        // Заполнение формы
         $("#firstName").setValue("Basil");
         $("#lastName").setValue("Pupkin");
         $("#userEmail").setValue("pupkin@basil.com");
         $$("#genterWrapper label").filterBy(text("Male")).first().click();
         $("#userNumber").setValue("0441234567");
 
-// Дата рождения
+        // Дата рождения
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("February");
         $(".react-datepicker__year-select").selectOption("1982");
         $(".react-datepicker__day--006:not(.react-datepicker__day--outside-month)").click();
 
-// Subjects
+        // Subjects
         $("#subjectsInput").setValue("Bio");
         $$(".subjects-auto-complete__option").findBy(text("Biology")).click();
 
-// Hobbies
+        // Hobbies
         $$("#hobbiesWrapper label").filterBy(text("Sports")).first().click();
         $$("#hobbiesWrapper label").filterBy(text("Reading")).first().click();
         $$("#hobbiesWrapper label").filterBy(text("Music")).first().click();
 
-
-
-// Upload picture
+        // Upload picture
         $("#uploadPicture").uploadFromClasspath("Foto 07.2024.jpg");
 
-// Address
+        // Address
         $("#currentAddress").setValue("Площадь Пушкина, пр-д Калатушкина, 6");
 
-// State и City с прокруткой и ожиданием
+        // State и City
         $("#state").scrollTo().shouldBe(interactable).click();
         $$("div[class*='-option']").findBy(text("Haryana")).click();
-
         $("#city").scrollTo().shouldBe(interactable).click();
         $$("div[class*='-option']").findBy(text("Karnal")).click();
 
-// Submit
+        // Submit
         $("#submit").scrollTo().click();
 
+        // Проверки (раздельные для каждого поля)
+        $(".modal-content").shouldBe(visible);
 
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        //  Проверка по таблице
+        $(".table-responsive")
+                .$(byText("Student Name")).parent().shouldHave(text("Basil Pupkin"));
+        $(".table-responsive")
+                .$(byText("Student Email")).parent().shouldHave(text("pupkin@basil.com"));
+        $(".table-responsive")
+                .$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive")
+                .$(byText("Mobile")).parent().shouldHave(text("0441234567"));
+        $(".table-responsive")
+                .$(byText("Date of Birth")).parent().shouldHave(text("06 February,1982"));
+        $(".table-responsive")
+                .$(byText("Subjects")).parent().shouldHave(text("Biology"));
+        $(".table-responsive")
+                .$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive")
+                .$(byText("Picture")).parent().shouldHave(text("Foto 07.2024.jpg"));
+        $(".table-responsive")
+                .$(byText("Address")).parent().shouldHave(text("Площадь Пушкина, пр-д Калатушкина, 6"));
+        $(".table-responsive")
+                .$(byText("State and City")).parent().shouldHave(text("Haryana Karnal"));
 
-        // Проверка данных в таблице
-        $$("table tbody tr").shouldHave(
-                exactTexts(
-                        "Student Name Basil Pupkin",
-                        "Student Email pupkin@basil.com",
-                        "Gender Male",
-                        "Mobile 0441234567",
-                        "Date of Birth 06 February,1982",
-                        "Subjects Biology",
-                        "Hobbies Sports, Reading, Music",
-                        "Picture Foto 07.2024.jpg",
-                        "Address Площадь Пушкина, пр-д Калатушкина, 6",
-                        "State and City Haryana Karnal"
-                )
-        );
-
+        // Закрытие модального окна
+        $("#closeLargeModal").click();
     }
 }
